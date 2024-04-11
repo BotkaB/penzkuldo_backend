3fd476b877c27e100b9077b8b2b2d30b22d332ca
BotkaB <botikabinaka@gmail.com>
2024-04-11 08:02:20 +0200

    végpont teszt datetime konvertálás string, teszt javítása

--- penzkuldo/tests/Feature/PenzmozgasTest.php
+++ penzkuldo/tests/Feature/PenzmozgasTest.php
@@ -8,6 +8,7 @@ use Tests\TestCase;
 use App\Models\Penzmozgas;
 use App\Models\Szamla;
 use App\Models\User;
+use Carbon\Carbon;
 
 class PenzmozgasTest extends TestCase
 {
@@ -33,10 +34,11 @@ class PenzmozgasTest extends TestCase
         $user2 = User::factory()->create();
         $szamla2 = Szamla::factory()->create();
 
-        $penzmozgas = Penzmozgas::factory()->make();
+        $penzmozgas = Penzmozgas::factory()->create();
+        $datum=Carbon::parse($penzmozgas->kuldes_idopont);
 
         
-    $this->withoutMiddleware()->get('/api/penzmozgas/' . $penzmozgas->kuldo_szamla .'/'.$penzmozgas->kuldes_idopont, )
+    $this->withoutMiddleware()->get('/api/penzmozgas/' . $penzmozgas->kuldo_szamla .'/'.$datum, )
     ->assertStatus(200);
 
     }
@@ -45,25 +47,37 @@ class PenzmozgasTest extends TestCase
     {
         $user1 = User::factory()->create();
         $szamla1 = Szamla::factory()->create();
-
+        $szamla1 = Szamla::find($szamla1->id);
+        $szamla1->egyenleg=321000;
+        $szamla1->save();
         $user2 = User::factory()->create();
         $szamla2 = Szamla::factory()->create();
-      
-        $response = $this->post('api/penzmozgas/', ['kuldo_szamla'=>$szamla1->id, 'cimzett_szamla'=>$szamla2->id, 'osszeg'=>'1', 'kuldes_idopont'=>'2022-02-22 19:56:47']);
+        $szamla2 = Szamla::find($szamla2->id);
+        $szamla2->egyenleg=1000;
+        $szamla2->save();
+        $response = $this->post('api/penzmozgas/', ['kuldo_szamla'=>$szamla1->id, 'cimzett_szamla'=>$szamla2->id, 'osszeg'=>'1000', 'kuldes_idopont'=>'2022-02-22 19:56:47']);
         $response->assertStatus(201);
+        $response = $this->post('api/penzmozgas/', ['kuldo_szamla'=>$szamla1->id, 'cimzett_szamla'=>$szamla2->id, 'osszeg'=>'411000', 'kuldes_idopont'=>'2022-02-22 19:56:47']);
+        $response->assertStatus(500);
     }
 
     public function test_penzmozgasModositApi()
     {
         $user1 = User::factory()->create();
         $szamla1 = Szamla::factory()->create();
-
+        $szamla1 = Szamla::find($szamla1->id);
+        $szamla1->egyenleg=320000;
+        $szamla1->save();
         $user2 = User::factory()->create();
         $szamla2 = Szamla::factory()->create();
+        $szamla2 = Szamla::find($szamla2->id);
+        $szamla2->egyenleg=30000000;
+        $szamla2->save();
+        $penzmozgas = Penzmozgas::factory()->create();
+        $datum=Carbon::parse($penzmozgas->kuldes_idopont);
 
-        $penzmozgas = Penzmozgas::factory()->make();
 
-        $response = $this->put('api/penzmozgas/'.$penzmozgas->kuldo_szamla .'/'.$penzmozgas->kuldes_idopont,['kuldo_szamla'=>$penzmozgas->kuldo_szamla, 'cimzett_szamla'=>$penzmozgas->cimzett_szamla, 'osszeg'=>'1000', 'kuldes_idopont'=>$penzmozgas->kuldes_idopont]);
+        $response = $this->withoutMiddleware()->put('api/penzmozgas/'.$penzmozgas->kuldo_szamla .'/'.$datum,['kuldo_szamla'=>$penzmozgas->kuldo_szamla, 'cimzett_szamla'=>$penzmozgas->cimzett_szamla, 'osszeg'=>'10000', 'kuldes_idopont'=>$penzmozgas->kuldes_idopont]);
 
         $response->assertStatus(200);
 }
