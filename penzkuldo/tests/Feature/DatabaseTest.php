0a95bb32a517ac4201c26565a36ee55bee9eb9fa
BotkaB <botikabinaka@gmail.com>
2024-04-11 15:06:20 +0200

    adatbázis teszt

---
+++ penzkuldo/tests/Feature/DatabaseTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Penzmozgas;
+use App\Models\Szamla;
+
+class DatabaseTest extends TestCase
+
+{
+    use RefreshDatabase;
+
+    /*
+     * A basic feature test example.
+   
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+      */
+
+      public function test_Db()
+      {
+        $this->seed();
+        User::factory()->count(10)->create();
+       
+       $this->assertDatabaseCount('users',30);
+
+       Szamla::factory()->count(15)->create();
+       $this->assertDatabaseCount('szamlas',45);
+
+       Penzmozgas::factory()->count(150)->create();
+       $this->assertDatabaseCount('penzmozgas',350);
+
+       User::factory()->create(
+        ['name'=>'Fodor Balázs',
+        'email'=>'otifh@hkbm.com',
+        'password'=>'pass11word' ]);
+     
+       $this->assertDatabaseHas('users', ['name'=>'Fodor Balázs']);
+    
+    }
+}
--- penzkuldo/tests/TestCase.php
+++ penzkuldo/tests/TestCase.php
@@ -5,9 +5,12 @@ use App\Models\User;
 use App\Models\Szamla;
 use App\Models\Penzmozgas;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 abstract class TestCase extends BaseTestCase
 {
+    use RefreshDatabase;
+
     protected function user()
 {
     return (User::factory()->make());
